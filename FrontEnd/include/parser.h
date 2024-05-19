ad3d92a67c6ca05429f30605e49c4c23a3eecd9d
Hmelnitskiy Anton <khmelnitskiianton@mail.ru>
2024-05-20 00:36:21 +0300

    clean git files

--- FrontEnd/include/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-Node_t* GetMain (Tokens_t* myTokens, BinaryTree_t* myTree);
-EnumOfErrors CopyVars(BinaryTree_t* myTree, Tokens_t* myTokens);
-
-#include "MyLangConfig/MyLangConfig.h"
-
-#endif
--- MiddleEnd/include/optimizier.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPTIMIZIER_H
-#define OPTIMIZIER_H
-
-#define ISNUM(side, value) ((side->Type == NUMBER) && (Compare(side->Value.Number, value)))
-#define TREE_CHANGED (myTree->ChangeOptimize = 1)
-
-EnumOfErrors TreeOptimize(BinaryTree_t* myTree);
-
-#endif
